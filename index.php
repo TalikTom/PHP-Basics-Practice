c7c906261e9cc6afee6d758b25969d38f7ed9d67
Luka Agic <lukagic@gmail.com>
2022-11-30 10:46:34 +0100

    Update score conditional statement

--- index.php
+++ index.php
@@ -90,17 +90,31 @@ var_dump(fmod($a, $b));
 // if / else /elseif / else if
 
 $score = 50;
+
 if($score >= 90) {
     echo 'A';
-    if($score >= 95) {
-        echo '+';
-    };
+        if($score >= 95) {
+            echo '+';
+        };
+
 } elseif($score >= 80) {
     echo 'B';
+        if($score >= 85) {
+            echo '+';
+        };
+
 } elseif($score >= 70) {
     echo 'C';
+        if($score >= 75) {
+            echo '+';
+        };
+
 } elseif($score >= 60) {
     echo 'D';
+        if($score >= 65) {
+            echo '+';
+        };
+        
 } else {
     echo 'F';
 };
