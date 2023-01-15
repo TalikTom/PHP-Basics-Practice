c76462ff56c4c05029c91a65991d293da33b1d8a
Luka Agic <agic.luke@gmail.com>
2023-01-15 15:21:26 +0100

    Add cookie page

---
+++ cookies.php
@@ -0,0 +1,54 @@
+<?php
+
+$counter = $_COOKIE['counter'] ?? 0;
+$counter = $counter + 1;
+setcookie('counter', $counter);
+$name = $_COOKIE['name'] ?? 0;
+setcookie('name', $name);
+$name = 'Luka';
+
+$color = $_COOKIE['color'] ?? null;
+$options = ['light', 'dark'];
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $color = $_POST['color'];
+    setcookie('color', $color, time() + 60 * 60, '/', '', false, true);
+}
+
+$scheme = (in_array($color, $options)) ? $color : 'dark';
+
+
+$message = 'Page views: ' . $counter;
+$message2 = 'This name was stored in a cookie: ' . $name;
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body class="<?= htmlspecialchars($scheme) ?>">
+<h1>Welcome</h1>
+<p>
+    <?= "$message <br>" ?>
+    <?= $message2 ?>
+
+    
+<p>Refresh this <a href="/cookies.php">Page</a> to see increase in views</p>
+</p>
+<form action="/cookies.php" method="POST">
+    <select name="color" id="color">
+        <option value="dark">Dark</option>
+        <option value="light">Light</option>
+    </select><br>
+    <input type="submit" value="Save">
+</form>
+
+</body>
+</html>
--- index.view.php
+++ index.view.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script src="https://code.highcharts.com/highcharts-more.js"></script>
@@ -14,7 +14,15 @@
     <title>Document</title>
 </head>
 <body>
+<nav class="nav_container">
+    <ul class="nav_bar">
+        <li><a href="/">Home</a></li>
+        <li><a href="/cookies.php">Cookies</a></li>
+        <li><a href="three.php">Three</a></li>
+    </ul>
+</nav>
 <h2>Get method - Multiplication table</h2>
+
 <form action="/" method="GET">
     <label for="row">Enter row value</label>
     <input type="text" value="" name="row">
@@ -29,17 +37,16 @@ $column = $_GET['column'] ?? 0;
 $row = $_GET['row'] ?? 0;
 
 echo '<table style="text-align: right;">';
-    for($i=1;$i<=$row;$i++){
+for ($i = 1; $i <= $row; $i++) {
     echo '<tr>';
-        for($j=1;$j<=$column;$j++)
-            {
-            echo '<td>' . $i * $j, '</td>';
-            }
-        echo '</tr>';
+    for ($j = 1; $j <= $column; $j++) {
+        echo '<td>' . $i * $j, '</td>';
     }
-    echo '</table>';
+    echo '</tr>';
+}
+echo '</table>';
 
-    ?>
+?>
 
 <h2>Post method - Cyclic matrix</h2>
 <form action="/" method="POST">
@@ -56,12 +63,11 @@ $rowPost = $_POST['row'] ?? 0;
 
 echo '<table style="text-align: right;">';
 
-for($i=1; $i<$rowPost+1; $i++){
+for ($i = 1; $i < $rowPost + 1; $i++) {
     echo '<tr>';
-        for($j=1; $j<$columnPost+1; $j++)
-        {
-            echo '<td>' . $j, '</td>';
-        }
+    for ($j = 1; $j < $columnPost + 1; $j++) {
+        echo '<td>' . $j, '</td>';
+    }
     echo '</tr>';
 }
 echo '</table>';
