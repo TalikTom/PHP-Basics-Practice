46d249b09c7bf7944fe9fd3732d50c32d85505ef
Luka Agic <agic.luke@gmail.com>
2023-01-16 16:38:46 +0100

    Required validation

--- index.view.php
+++ index.view.php
@@ -23,41 +23,13 @@
 </nav>
 <div class="main_container">
     <div class="main_content">
-        <h1>Tables - loops</h1>
-        <h2>Get method - Multiplication table</h2>
-
-        <form action="/" method="GET">
-            <label for="row">Enter row value</label>
-            <input type="number" value="" name="row" placeholder="1-10" max="10" min="1">
-            <label for="row">Enter column value</label>
-            <input type="number" value="" name="column" placeholder="1-10" max="10" min="1">
-            <input type="submit" class="btn">
-        </form>
-
-
-        <?php
-        $column = $_GET['column'] ?? 0;
-        $row = $_GET['row'] ?? 0;
-
-
-        echo '<table>';
-        for ($i = 1; $i <= $row; $i++) {
-            echo '<tr>';
-            for ($j = 1; $j <= $column; $j++) {
-                echo '<td>' . $i * $j, '</td>';
-            }
-            echo '</tr>';
-        }
-        echo '</table>';
-
-        ?>
 
         <h2>Post method - Spiral matrix - Bottom/right start</h2>
         <form action="/" method="POST">
             <label for="row">Enter row value</label>
-            <input type="number" value="" name="row" max="10" min="2" placeholder="2-10">
+            <input type="number" value="" name="row" max="10" min="2" placeholder="2-10" required>
             <label for="row">Enter column value</label>
-            <input type="number" value="" name="column" max="10" min="2" placeholder="2-10">
+            <input type="number" value="" name="column" max="10" min="2" placeholder="2-10" required>
             <input type="submit" class="btn">
         </form>
 
@@ -125,9 +97,9 @@
         <h2>Post method - Spiral matrix - Top/Left start</h2>
         <form action="/" method="POST">
             <label for="row">Enter row value</label>
-            <input type="number" value="" name="row2" max="10" min="2" placeholder="2-10">
+            <input type="number" value="" name="row2" max="10" min="2" placeholder="2-10" required>
             <label for="row">Enter column value</label>
-            <input type="number" value="" name="column2" max="10" min="2" placeholder="2-10">
+            <input type="number" value="" name="column2" max="10" min="2" placeholder="2-10" required>
             <input type="submit" class="btn">
         </form>
 
@@ -187,6 +159,35 @@
 
         }
 
+        echo '</table>';
+
+        ?>
+
+        <h1>Tables - loops</h1>
+        <h2>Get method - Multiplication table</h2>
+
+        <form action="/" method="GET">
+            <label for="row">Enter row value</label>
+            <input type="number" value="" name="row" placeholder="1-10" max="10" min="1" required>
+            <label for="row">Enter column value</label>
+            <input type="number" value="" name="column" placeholder="1-10" max="10" min="1" required>
+            <input type="submit" class="btn">
+        </form>
+
+
+        <?php
+        $column = $_GET['column'] ?? 0;
+        $row = $_GET['row'] ?? 0;
+
+
+        echo '<table>';
+        for ($i = 1; $i <= $row; $i++) {
+            echo '<tr>';
+            for ($j = 1; $j <= $column; $j++) {
+                echo '<td>' . $i * $j, '</td>';
+            }
+            echo '</tr>';
+        }
         echo '</table>';
 
         ?>
