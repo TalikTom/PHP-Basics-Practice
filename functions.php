547963edc187ef2e9d86a29f57c4f66f30aa2c16
Luka Agic <agic.luke@gmail.com>
2023-01-25 23:09:57 +0100

    Refactor code

--- functions.php
+++ functions.php
@@ -10,11 +10,14 @@ class OIB
 
     public static function oibMaker()
     {
-        $oib= '';
+
+        //generate first 10 numbers
+        $oib = '';
 
         for ($i = 0; $i < 10; $i++)
             $oib .= rand(0, 9);
 
+        //get 11. number by  ISO 7064 (MOD 11, 10)
         $a = 10;
 
         for ($i = 0; $i < 10; $i++) {
@@ -22,7 +25,9 @@ class OIB
             $a += (int)$oib[$i];
             $a %= 10;
 
-            if ( $a == 0 ) { $a = 10; }
+            if ($a == 0) {
+                $a = 10;
+            }
 
             $a *= 2;
             $a %= 11;
@@ -31,7 +36,9 @@ class OIB
 
         $kontrolni = 11 - $a;
 
+        //join first 10 with the 11. number
+        $oib .= $kontrolni === 10 ? 0 : $kontrolni;
 
-        return $oib . ($kontrolni === 10 ? 0 : $kontrolni);
+        return $oib;
     }
 }
