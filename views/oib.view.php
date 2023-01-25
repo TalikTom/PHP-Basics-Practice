6db51c002f89ffbccb2796968b94672ee405c791
Luka Agic <agic.luke@gmail.com>
2023-01-25 23:05:41 +0100

    Center content

--- views/oib.view.php
+++ views/oib.view.php
@@ -1,7 +1,11 @@
 <?php include 'partials/header.php'; ?>
 <?php include 'partials/nav.php'; ?>
+
+<div class="center-grid">
+    <h1>Press F5</h1>
 <?php
 $oib = $oib=OIB::oibMaker();
-echo $oib;
+echo '<p class="large-text">' . $oib . '</p>';
 ?>
+</div>
 <?php include 'partials/footer.php'?>
