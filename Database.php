6dfec2eb1ccd0aabca490153ec9694cd57691e4b
Luka Agic <agic.luke@gmail.com>
2023-01-25 15:41:14 +0100

    Refactor code

--- Database.php
+++ Database.php
@@ -1,21 +1,35 @@
 <?php
-class Database {
 
-public $connection;
-
-public function __construct() {
-$dsn = "mysql:host=localhost;port=3306;dbname=postit;user=root;charset=utf8mb4";
-$this->connection = new PDO($dsn);
-
-}
-
-public function query($query)
-
+class Database
 {
-$statement = $this -> connection ->prepare("$query");
-$statement -> execute();
-return $statement;
-}
+
+    public $connection;
+
+    public function __construct()
+    {
+
+        $config = [
+            'host' => 'localhost',
+            'port' => 3306,
+            'dbname' => 'postit',
+            'charset' => 'utf8mb4'
+        ];
+
+        http_build_query($config, '', ';');
+
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
+        $this->connection = new PDO($dsn, 'root', '', [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+
+    }
+
+    public function query($query)
+
+    {
+        $statement = $this->connection->prepare("$query");
+        $statement->execute();
+        return $statement;
+    }
 }
 
 ?>
