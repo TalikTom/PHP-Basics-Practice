f504eaa7f6bfddcb8ad813d648070faeb373decf
Luka Agic <agic.luke@gmail.com>
2023-01-25 15:30:51 +0100

    Seperate Database class into its own file

--- index.php
+++ index.php
@@ -1,32 +1,17 @@
 <?php
-
 declare(strict_types=1);
+require 'Database.php';
+
+
 
 // connect to the database, execute a query
 
-class Database {
 
-    public $connection;
-
-    public function __construct() {
-        $dsn = "mysql:host=localhost;port=3306;dbname=postit;user=root;charset=utf8mb4";
-        $this->connection = new PDO($dsn);
-
-    }
-
-    public function query($query)
-
-    {
-        $statement = $this -> connection ->prepare("$query");
-        $statement -> execute();
-        return $statement->fetchAll(PDO::FETCH_ASSOC);
-    }
-}
 
 
 $db = new Database();
 
-$notes = $db->query("select * from notes where user_id = 1");
+$notes = $db->query("select * from notes where user_id = 1")->fetchAll(PDO::FETCH_ASSOC);
 
 
 
