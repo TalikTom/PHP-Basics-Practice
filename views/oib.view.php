c5ddffcf9fc99573739934aaeb8508c9f7f894f0
Luka Agic <agic.luke@gmail.com>
2023-01-26 22:53:56 +0100

    Add textbox with copy button

--- views/oib.view.php
+++ views/oib.view.php
@@ -1,15 +1,55 @@
 <?php include 'partials/header.php'; ?>
 <?php include 'partials/nav.php'; ?>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $quantity = (int)$_POST['quantity'];
+
+    if ($quantity === 0) {
+        $quantity = '';
+    }
+}
+?>
+
 
 <div class="center-grid">
-    <h1>Press F5</h1>
+    <form action="/oib-generator" method="POST">
+        <label for="quantity">Enter how many OIBs you need:</label>
+        <input type="int" name="quantity" id="quantity" value="<?= htmlspecialchars($quantity) ?>">
+        <input type="submit">
+    </form>
+
+
+    <input type="text"
+           value=""
+           id="textbox">
+    <button onclick="copyText()">
+        Copy text
+    </button>
+    <script>
+        function copyText() {
+            let copyText = document.getElementById("textbox");
+            copyText.select();
+            document.execCommand("copy");
+            document.getElementById("message")
+                .innerHTML ="Copied the text!"
+        }
+    </script>
+    <p>
+        Click on the button to copy the text from the
+        text field.
+    </p>
+    <p id="message"></p>
+
 <?php
 $oib = OIB::oibMaker();
-$oibA = OIB::oibMore(50);
+echo '<p class="large-text">' . $oib . '</p>';
+
+$oibA = OIB::oibMore($quantity);
 foreach ($oibA as $o) {
     echo $o, '<br>';
 }
-echo '<p class="large-text">' . $oib . '</p>';
+
 ?>
 </div>
+
 <?php include 'partials/footer.php'?>
