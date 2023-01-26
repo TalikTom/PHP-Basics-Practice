b3f66335255226408562ee8afee2d0ea75473ae5
Luka Agic <agic.luke@gmail.com>
2023-01-26 23:49:00 +0100

    Style oib generator
    Style button, add border around generated oibs, center form on small and large screens

--- views/oib.view.php
+++ views/oib.view.php
@@ -1,34 +1,32 @@
 <?php include 'partials/header.php'; ?>
 <?php include 'partials/nav.php'; ?>
 <?php
+$quantity = '';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $quantity = (int)$_POST['quantity'];
 
-    if ($quantity === 0) {
-        $quantity = '';
-    } else {
-        $quantity = '';
-    }
 }
 ?>
 
 
 <div class="center-grid">
+    <h1>OIB Generator</h1>
+
     <?php
     $oib = OIB::oibMaker();
     echo '<p class="large-text" id="single">' . $oib . '</p>';
 
     ?>
-    <button type="button" onclick="copyEvent('single')">Copy to clipboard</button>
-    <form action="/oib-generator" method="POST">
+    <button class="btn" type="button" onclick="copyEvent('single')">Copy to clipboard</button>
+    <form class="generator" action="/oib-generator" method="POST">
         <label for="quantity">Enter how many OIBs you need:</label>
         <input type="int" name="quantity" id="quantity" value="<?= htmlspecialchars($quantity) ?>">
         <input type="submit" value="Get me OIBs!">
     </form>
 
 
-    <button type="button" onclick="copyEvent('copy')">Copy to clipboard</button>
-    <p id="copy"><?php
+    <button type="button" onclick="copyEvent('copy')" class="btn">Copy to clipboard</button>
+    <p id="copy" class="generator-text"><?php
         $oibA = OIB::oibMore($quantity);
         foreach ($oibA as $o) {
             echo $o, '<br>';
