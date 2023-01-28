494955aa64a97cd42c62025d8530af2299df8943
Luka Agic <agic.luke@gmail.com>
2023-01-28 22:40:37 +0100

    Add dynamic uri based on id in database
    Added dynamic links in displayed notes from the database, links now point to their user_id in the uri

--- views/notes.view.php
+++ views/notes.view.php
@@ -6,5 +6,11 @@
         echo '<li>' . $note['body'] . '</li>';
     }
     ?>
+
+    <?php foreach ($notes as $note) : ?>
+
+    <li><a href="/notes/?id=<?= $note['user_id'] ?>"><?= $note['body'] ?></a></li>
+
+    <?php endforeach; ?>
 </main>
 <?php include 'partials/footer.php'?>
