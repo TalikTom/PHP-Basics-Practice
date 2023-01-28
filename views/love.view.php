bf6dfdb49804ce6ad417f56820af7850e5c45fe0
Luka Agic <agic.luke@gmail.com>
2023-01-28 22:30:38 +0100

    Echo names and their char values

--- views/love.view.php
+++ views/love.view.php
@@ -5,12 +5,17 @@
 $name1 = '';
 $name2 = '';
 $namesTogether = '';
+$valid = '';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $name1 = $_POST['name1'];
     $name2 = $_POST['name2'];
 
     $name1 = mb_strtolower($name1);
     $name2 = mb_strtolower($name2);
+
+    $valid = is_string($name1);
+
+
 
     $name1 = str_replace(' ', '', $name1);
     $name2 = str_replace(' ', '', $name2);
@@ -23,24 +28,40 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $repeatedLettersCount = [];
 
     $repeatedLettersCount = LoveCalc::countValues($namesTogether, $namesTogether2);
-    $repeatedLettersCountName1 = array_slice($repeatedLettersCount,  0,mb_strlen($name1));
-    $repeatedLettersCountName2 = array_slice($repeatedLettersCount,  mb_strlen($name1), mb_strlen($name2));
+    $repeatedLettersCountName1 = array_slice($repeatedLettersCount, 0, mb_strlen($name1));
+    $repeatedLettersCountName2 = array_slice($repeatedLettersCount, mb_strlen($name1), mb_strlen($name2));
 
 }
 ?>
-<?php echo $name1, '<br>' ?>
-<?php echo $name2 , '<br>'?>
-<?php print_r($repeatedLettersCountName2) ?>
-<?php echo mb_strlen($name1) ?>
 
-<main class="center-grid" >
-    <form action="/love-calculator" method="POST">
-        <label for="name1" id="name1">Enter name of the first person</label>
-        <input type="text" name="name1">
-        <label for="name2" id="name2">Enter name of the second person</label>
-        <input type="text" name="name2">
-        <input type="submit">
-    </form>
-</main>
 
-<?php include 'partials/footer.php'?>
+<?php
+if ($valid) {
+    echo $name1, '<br>';
+
+    foreach ($repeatedLettersCountName1 as $i) {
+        echo $i;
+    }
+    echo '<br>' .$name2 . '<br>'  ;
+
+    foreach ($repeatedLettersCountName2 as $i) {
+        echo $i;
+    }
+} else {
+    echo 'please enter names using only characters a to ž';
+}
+?>
+
+
+
+    <main class="center-grid">
+        <form action="/love-calculator" method="POST">
+            <label for="name1" id="name1">Enter name of the first person</label>
+            <input type="text" name="name1">
+            <label for="name2" id="name2">Enter name of the second person</label>
+            <input type="text" name="name2">
+            <input type="submit">
+        </form>
+    </main>
+
+<?php include 'partials/footer.php' ?>
