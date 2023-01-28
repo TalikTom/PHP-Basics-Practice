de59d34340df625262e2d980e8b29490feb91277
Luka Agic <agic.luke@gmail.com>
2023-01-28 16:33:56 +0100

    Clean inputs and join them

--- views/love.view.php
+++ views/love.view.php
@@ -3,20 +3,30 @@
 <?php
 $name1 = '';
 $name2 = '';
+$namesTogether = '';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $name1 = (int)$_POST['name1'];
-    $name2 = (int)$_POST['name2'];
+    $name1 = $_POST['name1'];
+    $name2 = $_POST['name2'];
+
+    $name1 = mb_strtolower($name1);
+    $name2 = mb_strtolower($name2);
+
+    $name1 = str_replace(' ', '', $name1);
+    $name2 = str_replace(' ', '', $name2);
+
+    $namesTogether = $name1 . $name2;
 
 }
 ?>
-?>
 
+<?= $namesTogether ?>
 <main class="center-grid" >
-    <form action="/love-calculator">
+    <form action="/love-calculator" method="POST">
         <label for="name1" id="name1">Enter name of the first person</label>
         <input type="text" name="name1">
         <label for="name2" id="name2">Enter name of the second person</label>
         <input type="text" name="name2">
+        <input type="submit">
     </form>
 </main>
 
