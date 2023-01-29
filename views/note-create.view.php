5a0d4f93a562b70daa60cd381a9171b82accd19a
Luka Agic <agic.luke@gmail.com>
2023-01-30 00:45:43 +0100

    Add form to insert into database via POST

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -2,5 +2,10 @@
 <?php include 'partials/nav.php'; ?>
     <main class="container">
         <h1>Create a note</h1>
+        <form method="POST">
+            <label for="body">Enter message</label>
+            <input type="text" id="body" name="body">
+            <input type="submit">
+        </form>
     </main>
 <?php include 'partials/footer.php' ?>
