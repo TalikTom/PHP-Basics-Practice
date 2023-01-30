c2e95b30596a9873e22d373f8e85bf9ed680e5c5
Luka Agic <agic.luke@gmail.com>
2023-01-30 01:38:34 +0100

    Add validation, check if the field is empty

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -7,11 +7,22 @@ $db = new Database($config['database']);
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes(body,user_id) VALUES(:body, :user_id)',
-    [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
+
+    $errors = [];
+
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes(body,user_id) VALUES(:body, :user_id)',
+            [
+                'body' => $_POST['body'],
+                'user_id' => 1
+            ]);
+    }
+
+
 }
 
 require 'views/note-create.view.php';
