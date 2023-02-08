122dc788fd8764d53aeadaddd5fa64bdcc7177c6
Luka Agic <agic.luke@gmail.com>
2023-02-08 23:47:28 +0100

    Add login function

--- views/login.view.php
+++ views/login.view.php
@@ -5,3 +5,9 @@ $logged_in = $_SESSION['logged_in'] ?? false;
 $email = 'luka@gmail.com';
 $password = 'password';
 
+function login() {
+
+    session_regenerate_id(true);
+    $_SESSION['logged_in'] = true;
+
+}
